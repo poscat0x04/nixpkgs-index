b6434f253e225c0c68619e16c7b79a79af96cdee
Poscat <poscat@mail.poscat.moe>
2020-12-30 14:54:51 +0800

    Add parseRequest support to the Http effect

--- src/Control/Effect/Http.hs
+++ src/Control/Effect/Http.hs
@@ -6,13 +6,19 @@ module Control.Effect.Http where
 import Control.Effect
 import Data.ByteString.Lazy (ByteString)
 import Network.HTTP.Client
+import qualified Network.HTTP.Client as C
 
 data Http :: Effect where
+  ParseRequest :: String -> Http m Request
   PerformRequest :: Request -> Http m (Response ByteString)
+
+parseRequest :: Eff Http m => String -> m Request
+parseRequest s = send $ ParseRequest s
 
 performRequest :: Eff Http m => Request -> m (Response ByteString)
 performRequest r = send $ PerformRequest r
 
 httpToIO :: Eff (Embed IO) m => Manager -> SimpleInterpreterFor Http m
-httpToIO manager = interpretSimple $
-  \(PerformRequest r) -> embed $ httpLbs r manager
+httpToIO manager = interpretSimple $ \case
+  ParseRequest s -> embed $ C.parseRequest @IO s
+  PerformRequest r -> embed $ httpLbs r manager
